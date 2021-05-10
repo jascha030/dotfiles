fe93358767042a4248d212b34c0acf276f8d52d5
Jascha030 <contact@jaschavanaalst.nl>
2021-05-10 12:40:01 +0200

    Started function to run brew install and automatically add to Brewfile

--- Shell/custom_functions.zsh
+++ Shell/custom_functions.zsh
@@ -15,3 +15,49 @@ function dotf(){
 	edit "$NAVDIR"
 }
 
+function brwsave() {
+	COMMAND="brew install "
+	
+	echo "What kind of package are we installing?"
+	
+	select CASK in "Binary" "Cask"
+	do
+		case $CASK in 
+			"Cask")
+			COMMAND="$COMMAND --cask"
+			FILE_PREFIX="cask"
+			break;
+			;;
+			"Binary")
+			FILE_PREFIX="brew"
+			break;
+			;;
+		*)
+			echo "Error! This is not an option..."
+			&>2 exit 1
+			break;
+			;;
+		esac
+	done
+
+	echo "Which one? \n"
+	read PACKAGE
+	echo "Okay"
+	
+	if ! ($COMMAND $PACKAGE); then
+		echo "Something went wrong, could not install $PACKAGE..."
+		exit 1;
+	fi
+	
+
+	if read -q "choice? Save to Brewfile? Y/y: "; then
+        	echo 
+		(echo "$FILE_PREFIX \"$PACKAGE\" \n")
+    	else
+        	echo
+        	echo "Okay, just trying it out hmm."
+    	fi
+
+	echo "Bye!"
+}
+
