b6dcdc4b4a8de943f6601daba87a4117f2ba8b8d
Jascha030 <contact@jaschavanaalst.nl>
2021-05-10 12:39:07 +0200

    Added treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,7 +4,17 @@ return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	use 'neovim/nvim-lspconfig'
 	use 'marko-cerovac/material.nvim'
+	
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } 
+
 	use 'onsails/lspkind-nvim'
+	
+	use {
+		'hoob3rt/lualine.nvim',	
+  		requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	}
+
 	use 'kyazdani42/nvim-tree.lua'
+
 end)
 
