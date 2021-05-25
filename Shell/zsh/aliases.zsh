b8a915126fadfe4bd88863f5a8070b4ee966a06f
Jascha030 <contact@jaschavanaalst.nl>
2021-05-25 22:58:15 +0200

    Move zsh files to shell/zsh

---
+++ Shell/zsh/aliases.zsh
@@ -0,0 +1,99 @@
+# Ali(J)asses
+alias edit="${EDITOR} $1"
+alias zshconfig="edit $DOTFILES/.zshrc"
+alias zshalias="edit $DOTFILES/shell/aliases.zsh"
+alias ohmyzsh="nvim $HOME/.oh-my-zsh"
+
+# Navigation
+alias rlsh="source $HOME/.zshrc"
+alias rr="rlsh"
+alias x="exit"
+alias c="clear"
+alias ll="colorls -Ahl --color --group-directories-first"
+alias confdir="cd $HOME/.config"
+
+# Clean and update Brew
+alias brewup="brew update && brew outdated; brew upgrade && brew cleanup; brew doctor"
+
+# Brew install and add to Brewfile
+# alias brewsave="brew install $1; echo "${1} \n" >> $DOTFILES/Brewfile;"
+# alias casksave=brew install $1 --cask; echo "${$1} \n" >> $DOTFILES/Brewfiles;
+
+# Faster Time Machine Backup
+alias tm-auto="sudo tmutil startbackup --auto; tmutil status"
+
+# Remove valet.sock and restart.
+alias valetrm="rm $HOME/.config/valet/valet.sock"
+alias valetr="valetrm && valet restart" # Make a symbolic link in current dir.
+alias linkdir="ln -s $PWD/"
+
+# Chrome with just a little bit less RAMpage.
+alias chrome="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --args --process-per-site"
+alias browser="chrome"
+alias chromi="chrome -incognito"
+
+# Open current dir in...
+alias kraken='$(open gitkraken://repo/$PWD)' # Gitkraken
+alias pstorm="phpstorm ." # Phpstorm
+alias wstorm="webstorm ." # Webstorm
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# Working dir
+alias dev="cd ~/.projx"
+alias temp="cd ~/.projx/temp"
+
+alias dotfiles="cd $DOTFILES"
+alias dfs="dotfiles"
+alias library="cd $HOME/Library"
+
+alias wwwd="cd $HOME/.projx/www"
+alias remotesites="cd $HOME/.projx/www/Remotes"
+alias sbsites="cd $HOME/.projx/www/socialbrothers"
+
+# alias sb="cd ~/SocialBrothers"
+# alias programming="cd ~/Programming"
+
+# PHP
+alias cc="&& composer "
+alias cpsrcl="composer clear-cache"
+alias cpsrrm="rm -rf vendor/ composer.lock"
+alias cpsrfrup="cpscl cc u"
+alias cpsrfri="cpscl cc i"
+alias cpsrfresh="comprm && cpsfri"
+alias cpsndev="composer install --no-dev"
+
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
+
+# Suffix Aliases
+alias -s php=pstorm
+alias -s {.zshrc,zsh,sh,json,yaml,.env}=nvim
+alias -s {cs,ts,html}=code
+
+# Templates
+alias glbgitig="cat $DOTFILES/.gitignore | pbcopy"
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias cpssh="copyssh"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias ccdir="echo $PWD | pbcopy"
+
