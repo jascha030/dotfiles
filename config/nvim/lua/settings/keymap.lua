c5ce3392c0c4514fdde66482a572c47326c0ec98
Jascha030 <contact@jaschavanaalst.nl>
2021-07-05 14:12:09 +0200

    Add keymap for telescope

--- config/nvim/lua/settings/keymap.lua
+++ config/nvim/lua/settings/keymap.lua
@@ -1,3 +1,5 @@
 local a = vim.api
 
 a.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>f', ":lua require('plugins.telescope').project_files()<CR>", {noremap = true, silent = true})
+
