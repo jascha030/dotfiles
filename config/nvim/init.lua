45b7f5ca9238b27e92c20396167f84b825ce524a
Jascha030 <contact@jaschavanaalst.nl>
2021-07-27 22:35:30 +0200

    Move packer init to plugin module

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,4 @@
 vim.g.mapleader = '<space>'
-
-local execute = vim.api.nvim_command
-local fn = vim.fn
-local g = vim.g
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
-
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 require('plugins')
